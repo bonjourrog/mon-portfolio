f0b0943fe53e726108f5140924e98b5fe4fe8e9c
Rogelio Beltrán <rogelio.beltran@radek.mx>
2024-04-29 22:35:55 -0700

    feat: create Home component

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import './Home.css';
+import { HomeProps } from './props';
+
+const Home: React.FC<HomeProps> = ()=>{
+    return <div className='home'>
+        kjhjk
+    </div>
+}
+export default Home;
