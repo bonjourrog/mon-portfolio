b8ea9cc4fba585b84c1459029a395fb39225784b
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-06-08 18:38:44 -0700

    feat: create header

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,7 @@
+import './Header.css';
+import { HeaderProps } from './props';
+
+const Header: React.FC<HeaderProps> = ()=>{
+    return <header className='header'></header>
+}
+export default Header;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import Header from '../../Components/Header';
 import Hero from './Components/Hero';
 import './Home.css';
 import { HomeProps } from './props';
 
 const Home: React.FC<HomeProps> = ()=>{
     return <div className='home'>
+        <Header/>
         <Hero/>
     </div>
 }
