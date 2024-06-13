47294d1ec22d3ef874b835d3b9e57e39d7b05368
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-06-12 22:55:46 -0700

    feat: create contact component

---
+++ src/Pages/Home/Components/Contact/index.tsx
@@ -0,0 +1,46 @@
+import './Contact.css';
+import { ContactProps } from './Contact.props';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { FaLinkedin, FaGithubSquare, FaInstagramSquare } from "react-icons/fa";
+
+const Contact:React.FC<ContactProps> = ()=>{
+    const validateSchema = Yup.object().shape({
+        name:Yup.string().required("Ingrese su nombre").min(3, "El nombre es demasiado corto"),
+        email:Yup.string().required("Ingrese un correo").email("Ingrese un correo valido"),
+        message: Yup.string().required("Ingrese un mensaje").min(10, "El mensaje es demasiado corto")
+    });
+    return <section className='contact'>
+    <Formik initialValues={{name:'', email:'', message:''}} validationSchema={validateSchema} onSubmit={(values, {resetForm, setSubmitting})=>{}}>
+        <div>
+            <Form className='form'>
+                <h2 className='contact__headline'>Contacto</h2>
+                <Field className='form__field' name='name' type='text' placeholder='Nombre'/>
+                <ErrorMessage component={'span'} className='error-msg' name='name'/>
+                <br />
+                <Field className='form__field' name='email' type='email' placeholder='Correo'/>
+                <ErrorMessage component={'span'} className='error-msg' name='email'/>
+                <br />
+                <Field className='form__field form__field--textarea' name='message' type='text' component='textarea' placeholder='Mensaje'/>
+                <ErrorMessage component={'span'} className='error-msg' name='message'/>
+            </Form>
+            <div className='socials'>
+                <h2>Socials</h2>
+                <div>
+                    <a href="">
+                        <FaLinkedin className='socials__icon'/>
+                    </a>
+                    <a href="">
+                        <FaGithubSquare className='socials__icon'/>
+                    </a>
+                    <a href="">
+                        <FaInstagramSquare className='socials__icon'/>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </Formik>
+</section>
+}
+export default Contact;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,16 +1,19 @@
+import './Home.css';
 import Header from '../../Components/Header';
 import Experience from './Components/Experience';
 import Hero from './Components/Hero';
 import Projects from './Components/Projects';
-import './Home.css';
 import { HomeProps } from './props';
+import Contact from './Components/Contact';
 
 const Home: React.FC<HomeProps> = ()=>{
+    
     return <div className='home' style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
         <Header/>
         <Hero/>
         <Projects/>
         <Experience/>
+        <Contact/>
     </div>
 }
 export default Home;
