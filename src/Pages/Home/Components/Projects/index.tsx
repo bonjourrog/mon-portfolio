5410e82d0745e027d1e4938aad13ea0ee800b7aa
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-06-26 21:29:44 -0700

    feat: add links to the project images

--- src/Pages/Home/Components/Projects/index.tsx
+++ src/Pages/Home/Components/Projects/index.tsx
@@ -11,16 +11,24 @@ const Projects: React.FC<ProjectsProps> = ()=>{
             <FaLongArrowAltRight className='project__icon'/>
         </li>
         <li className='project'>
-            <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718214667/portfolio/prurfcocftiiv5t79rbd.jpg" alt="" />
+            <a href="https://bluebaycarrental.com/" target='_blank'>
+                <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718214667/portfolio/prurfcocftiiv5t79rbd.jpg" alt="" />
+            </a>
         </li>
         <li className='project'>
-            <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718214840/portfolio/g03fb6t9tpetbswqfhzd.jpg" alt="" />
+            <a href="https://joyatinmobiliaria.com/" target='_blank'>
+                <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718214840/portfolio/g03fb6t9tpetbswqfhzd.jpg" alt="" />
+            </a>
         </li>
         <li className='project'>
-            <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718143364/portfolio/obgizhwqtg4rnssqhhzq.jpg" alt="" />
+            <a href="https://elcomedero.com.mx/" target='_blank'>
+                <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718143364/portfolio/obgizhwqtg4rnssqhhzq.jpg" alt="" />
+            </a>
         </li>
         <li className='project'>
-            <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718215116/portfolio/fsteylcuab6auhaxirct.jpg" alt="" />
+            <a href="https://lafatburger.com/" target='_blank'>
+                <img src="https://res.cloudinary.com/dbquvfvvk/image/upload/v1718215116/portfolio/fsteylcuab6auhaxirct.jpg" alt="" />
+            </a>
         </li>
     </ul>
 </section>
